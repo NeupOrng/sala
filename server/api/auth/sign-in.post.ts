b583eca682718a6a16b117a9bba45a24ff446f0a
orng <orng.neu@ascendcorp.com>
2025-06-27 13:28:34 +0700

    implement redis

--- server/api/auth/sign-in.post.ts
+++ server/api/auth/sign-in.post.ts
@@ -1,11 +1,14 @@
-import { compare } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import jwt from "jsonwebtoken";
 import { eq } from "drizzle-orm";
 import { ForbiddenException } from "~/server/dto/response/exception/forbidden";
 import { Users } from "~/server/schema/user";
 import { db } from "~/server/utils/db";
+import redis from "~/server/utils/redis";
+import { RedisKey } from "~/server/dto/constant/redis-key";
 
 const JWT_SECRET = process.env.JWT_SECRET || "your-secret";
+const SALT = process.env.SALT || 10;
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
@@ -18,7 +21,7 @@ export default defineEventHandler(async (event) => {
         .limit(1);
 
     if (!user || !(await compare(password, user.passwordHash))) {
-        throw new ForbiddenException("Invalid email or password");
+        throw new ForbiddenException("Invalid username or password");
     }
     const token = jwt.sign(
         { id: user.id, email: user.username, role: user.role },
@@ -34,6 +37,9 @@ export default defineEventHandler(async (event) => {
         sameSite: "lax",
         maxAge: 60 * 60 * 24 * 7, // 7 days
     });
+
+    redis.set(`${RedisKey.authToken}:${token}`, JSON.stringify(user), "EX", 60 * 60 *24 * 7)
+    redis.set(`${RedisKey.authUser}:${user.id}`, token, "EX", 60 * 60 *24 * 7)
 
     return { success: true };
 });
---
+++ server/dto/constant/redis-key.ts
@@ -0,0 +1,4 @@
+export const RedisKey = {
+    authToken: 'auth-token',
+    authUser: 'auth-user'
+}
