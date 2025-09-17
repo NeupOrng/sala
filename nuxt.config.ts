12997a081e0afd5ae08bbe956a6d1f57d52e549c
orng <orng.neu@ascendcorp.com>
2025-09-17 16:44:17 +0700

    add nitro config for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,25 @@
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@pinia/nuxt', 'shadcn-nuxt'],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
-  },
-  vite: {
-    plugins: [
-      tailwindcss()
-    ],
-    resolve: {
-      alias: {
-        'cloudflare:sockets': '/empty-module.js',
-      },
+    nitro: {
+        preset: "cloudflare-pages",
     },
-    optimizeDeps: {
-      exclude: ['pg'],
+    compatibilityDate: "2025-05-15",
+    devtools: { enabled: true },
+    css: ["~/assets/css/main.css"],
+    modules: ["@pinia/nuxt", "shadcn-nuxt"],
+    shadcn: {
+        prefix: "",
+        componentDir: "./components/ui",
     },
-  }
-})
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "cloudflare:sockets": "/empty-module.js",
+            },
+        },
+        optimizeDeps: {
+            exclude: ["pg"],
+        },
+    },
+});
