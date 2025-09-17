f2fa4ac6900eeef8ce8fd95412341b408c59ad63
orng <orng.neu@ascendcorp.com>
2025-09-17 17:41:59 +0700

    change db

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,9 +1,10 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from '../schema';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const sql = postgres(process.env.DATABASE_URL ?? "postgres://postgres:postgres@127.0.0.1:5432/sala", {
+    ssl: 'require',
+  });
 
 // Create Drizzle client
-export const db = drizzle(pool);
+export const db = drizzle(sql, { schema });
